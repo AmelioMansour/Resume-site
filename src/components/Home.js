ec895c2f1caecb69d3921f62de2d7e48a02133ab
AmelioMansour <ameliomansour@oakland.edu>
2023-09-02 00:46:15 -0400

    attempt at using updated lottie package

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,24 @@
 import React from "react";
-import Lottie from 'react-lottie';
+import Lottie from "lottie-react";
 import animationData from '../assets/lotti.json';
-
 import headshot from '../assets/headshot.jpeg';
 
 function Home() {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
-    }
-  };
-
   return (
     <div className="flex">
       <div className="mrpic">
         <img src={headshot} alt="headshot" />
-        <div className="special1" id="name" className="absolute top-1/4 left-1/4 text-white animate-slideInDown">Amelio Mansour</div>
-        <div className="special2" className="absolute top-1/4 left-1/4 text-white animate-slideInDown" id="title">Software Developer</div>
+        <div class="special1" id="name" className="absolute top-1/4 left-1/4 text-white animate-slideInDown">Amelio Mansour</div>
+        <div class="special2" className="absolute top-1/4 left-1/4 text-white animate-slideInDown" id="title">Software Developer</div>
       </div>
 
-      <div className="flex items-start w-full">
+      <div className="flex items-start">
         <Lottie
-          options={defaultOptions}
-          height={400}
-          width={400}
-          alt="lottie animation of software dev"
-          className="mr-4"
-          style={{ border: "1px solid black" }}
+          animationData={animationData}
+          style={{ width: 400, height: 400, border: "1px solid black" }}
         />
-
-
-        <div className="ml-6 flex flex-col" style={{ border: "1px solid black" }}>
+       
+        <div className="ml-6 flex flex-col" style={{border: "1px solid black"}} > 
           <p>Sterling Heights, MI | ameliomansour612@gmail.com | (586) 233-3132</p>
           <p>Career Focus: Software Development/Engineer Role</p>
           <p>Information Technology Senior with development skills, previous internship experience, and a knowledge of a variety of
@@ -43,8 +28,6 @@ function Home() {
       </div>
     </div>
   );
-
-
 }
 
 export default Home;
